35b5d0473b206ed8f5e060904f716da700f9f325
LeonAvancini <avancinileon@gmail.com>
2021-03-11 23:51:22 -0300

    added datepicker and get data from the Nasa Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import styled from "styled-components";
+import "react-datepicker/dist/react-datepicker.css";
+import { formatDate } from "./utils/Dates";
 
-function App() {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  color: black;
+`;
+
+const ImageTitle = styled.h1`
+  color: black;
+`;
+
+const DatePickerStyled = styled(DatePicker)`
+  border-radius: 10px;
+  font-size: 25px;
+`;
+
+const Image = styled.img`
+  border-radius: 5px;
+`;
+
+export default () => {
+  const [test, setTest] = useState<any>({});
+  const [startDate, setStartDate] = useState<Date | null | undefined>(
+    new Date()
+  );
+
+  const dateFormatted = formatDate(startDate ?? new Date());
+
+  //FIXME: API DATA WEBSITE === https://api.nasa.gov/index.html
+  const API_KEY: string = "api_key=J9c8DvGBTzcqvhFd8UPQIJqhzJ80Ractk3JBOiRx";
+  const baseUrl = "https://api.nasa.gov";
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `${baseUrl}/planetary/apod?date=${dateFormatted}&${API_KEY}`
+    );
+    const data = await response.json();
+    setTest(data);
+    console.log("datos", test);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [dateFormatted]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Title onClick={() => fetchData()}>Api Connected Website</Title>{" "}
+        <Title>{`Astronomy Picture of the ${dateFormatted}`}</Title>
+        <DatePickerStyled
+          selected={startDate}
+          onChange={(date: Date) => setStartDate(date)}
+        />
+        <ImageTitle>{test.title}</ImageTitle>
+          <Image width="50%" height="50%" src={`${test.url}`} />
+      </Container>
     </div>
   );
-}
-
-export default App;
+};
